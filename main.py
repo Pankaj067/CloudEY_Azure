ee614d806d4d7f7a8494f6eaca3ba4a102844f71
Pankaj Kumar <pankaj786067@gmail.com>
2019-01-07 12:05:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return render_template('Presentation.html')
+  return render_template('Test.html')
 
 @app.route('/AI')
 def AIModule():
