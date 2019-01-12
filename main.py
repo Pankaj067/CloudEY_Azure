6e609cdc3ede0369a5fb50133318f65dbda080f0
Pankaj Kumar <pankaj786067@gmail.com>
2019-01-12 20:34:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,24 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return ('Python configuration is sucessfull')
+  return 'Python configuration is sucessfull'
 
 if __name__ == '__main__':
   app.run()
-
-  
-  
-  
-  '''
-  @app.route('/')
-def hello_world():
-  return render_template('Presentation.html')
-
-@app.route('/AI')
-def AIModule():
-    return render_template('ForTesting.html')
-
-if __name__ == '__main__':
-  app.run()
- 
-  '''
